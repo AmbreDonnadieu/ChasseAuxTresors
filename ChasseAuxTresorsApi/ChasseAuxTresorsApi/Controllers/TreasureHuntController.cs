c577c1ad92a953ecce993e3ea53f13a5ccb809f7
Ambre Donnadieu <43542400+AmbreDonnadieu@users.noreply.github.com>
2023-09-27 14:04:19 +0200

    some comment

--- ChasseAuxTresorsApi/ChasseAuxTresorsApi/Controllers/TreasureHuntController.cs
+++ ChasseAuxTresorsApi/ChasseAuxTresorsApi/Controllers/TreasureHuntController.cs
@@ -24,20 +24,29 @@ namespace ChasseAuxTresorsApi.Controllers
         [Route("")]
         public async Task<FileResult> GetTreasureHuntResultFromFile(IFormFile file)
         {
+            // we convert file to list of strings to facilitate treatement after
             var alllines = await _fileService.ConvertFileToListStringAsync(file);
+
+            //from this list, we create the map
             var map = _mapService.CreateMapFromListOfLines(alllines);
+            //then we create and place the adventurers
             var adventurers = _adventurerService.PlaceThemOnMap(alllines, map);
 
+            // we will write in a string every state of the map and first, we write initial state
             var allMapTurns = $"Initialization : \n{_mapService.WriteMap(map)}\n\n";
+
+            //if there is no adventurers, there will be no evolution so we return the result directly
             if (!adventurers.Any())
             {
                 allMapTurns = $"{allMapTurns} There were no adventurers on the map. It stays at initialization state.";
                 return File(Encoding.UTF8.GetBytes(allMapTurns), "application/txt", "result.txt");
             }
 
+            // we get how many turn there will be by checking in diretly the mouvement of all adventurer
             var numberOfTurn = adventurers.Select(a => a.Mouvements.Length).Max();
             for (var i = 0; i <= numberOfTurn; i++)
             {
+                // at each turn, we do the action of each adventurer
                 foreach (var adv in adventurers)
                 {
                     _adventurerService.DoNextAction(adv, map);
@@ -45,6 +54,7 @@ namespace ChasseAuxTresorsApi.Controllers
                 allMapTurns = $"{allMapTurns} TURN {i} :\n{_mapService.WriteMap(map)}\n\n";
             }
 
+            // when all turns were done, we throw the result
             return File(Encoding.UTF8.GetBytes(allMapTurns), "application/txt", "result.txt");
         }
     }
--- ChasseAuxTresorsApi/ChasseAuxTresorsApi/Services/FileService.cs
+++ ChasseAuxTresorsApi/ChasseAuxTresorsApi/Services/FileService.cs
@@ -8,6 +8,10 @@ namespace ChasseAuxTresorsApi.Services
 
         public async Task<List<string>> ConvertFileToListStringAsync(IFormFile file)
         {
+            //the code below was made for text file only, so we throw an error in other case
+            if (Path.GetExtension(file.FileName) != ".txt")
+                throw new Exception("We don't accept this type of file. Try using a text file instead.");
+
             var allLines = new List<string>();
             using (var reader = new StreamReader(file.OpenReadStream()))
             {
